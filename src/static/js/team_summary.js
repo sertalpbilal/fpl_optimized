919898cc28cdd2ac70d19ae477732b15411bde33
Sertalp B. Cay <sertalpbilal@gmail.com>
2020-12-23 10:06:51 -0500

    GW Summary is working now

--- src/static/js/team_summary.js
+++ src/static/js/team_summary.js
@@ -48,6 +48,8 @@ var app = new Vue({
             this.rp_data = data;
         },
         generateList() {
+
+            // PART 1: PRIOR DATA
             if (this.team_data.length == 0) { return; }
             if (this.rp_data == 0) { return; }
             let pts = this.xp_data;
@@ -59,6 +61,8 @@ var app = new Vue({
             let captain = team.picks.filter(i => i.is_captain)[0].element;
             let lineup = team.picks.filter(i => i.multiplier >= 1).map(i => i.element);
             let squad = team.picks.map(i => i.element);
+            let rp = Object.fromEntries(this.rp_data);
+
             pts.forEach((e) => {
                 // e.info = els[e.player_id];
                 e.element_type = els[e.player_id]['element_type'];
@@ -66,10 +70,12 @@ var app = new Vue({
                 e.ownership = els[e.player_id]['selected_by_percent'];
                 e.lineup = lineup.includes(parseInt(e.player_id));
                 e.squad = squad.includes(parseInt(e.player_id));
+                e.captain = (e.player_id == captain);
                 e.xp_owned = (1 - e.ownership / 100) * e.points_md;
                 e.xp_non_owned = -e.ownership / 100 * e.points_md;
                 e.net_xp = ((e.lineup == 1) - e.ownership / 100) * e.points_md;
                 e.threat = false;
+                e.stats = rp[e.player_id];
             });
             let sorted_players = Object.entries(pts).sort((a, b) => {
                 if (a[1].squad == b[1].squad) {
@@ -81,10 +87,11 @@ var app = new Vue({
                     return (a[1].squad < b[1].squad) * 2 - 1;
                 }
             });
-            for (let i of sorted_players.slice(-10)) {
+            for (let i of sorted_players.slice(-5)) {
                 i[1].threat = true;
             };
             this.sorted_data = sorted_players;
+
         }
     },
     computed: {
@@ -145,6 +152,14 @@ var app = new Vue({
             let net_change = parseFloat(lineup_xp) + parseFloat(other_xp);
             let change = "" + net_change < 0 ? net_change.toFixed(2) : "+" + net_change.toFixed(2);
             return change;
+        },
+        aftermath: function() {
+            if (this.team_data.length == 0) { return {}; }
+            let gw_xp = this.prior_data.filter(j => j[1].lineup).map(j => j[1].points_md * (1 - j[1].ownership / 100)).reduce((a, b) => a + b, 0);
+            let gw_rp = this.prior_data.filter(j => j[1].lineup).map(j => j[1].stats.total_points * (1 - j[1].ownership / 100)).reduce((a, b) => a + b, 0);
+            let fpl_xp = this.prior_data.filter(j => j[1].lineup == false).map(j => j[1].points_md * (j[1].ownership / 100)).reduce((a, b) => a + b, 0);
+            let fpl_rp = this.prior_data.filter(j => j[1].lineup == false).map(j => j[1].stats.total_points * (j[1].ownership / 100)).reduce((a, b) => a + b, 0);
+            return { 'net_xp': gw_xp, 'net_rp': gw_rp, 'fpl_xp': fpl_xp, 'fpl_rp': fpl_rp }
         }
     }
 })
@@ -200,7 +215,8 @@ function load_gw() {
         },
         success: function(data) {
             let elemvals = JSON.parse(data);
-            app.saveRPData(elemvals);
+            let rp_data = elemvals.elements.map(i => [i.id, i.stats])
+            app.saveRPData(rp_data);
             app.generateList();
             app.$nextTick(() => {
                 $(".plot").empty();
@@ -353,10 +369,8 @@ function plot_bubble_xp_own_prior() {
         own_color = "white";
         threat_color = "white";
         if (d[1].squad == true) {
-            have_line = '<tr><td class="text-right">Type</td><td>Squad</td></tr>'
             name_color = own_color = "#6fcfd6";
         } else if (d[1].threat == true) {
-            have_line = '<tr><td class="text-right">Type</td><td>Threat</td></tr>'
             name_color = threat_color = "#de6363";
         }
         tooltip
@@ -382,7 +396,7 @@ function plot_bubble_xp_own_prior() {
     }
 
     // Guidelines
-    lines = [3, 5, 10, 25, 50];
+    lines = [5, 10, 25, 50];
     svg.append('g')
         .selectAll()
         .data(lines)
@@ -400,7 +414,8 @@ function plot_bubble_xp_own_prior() {
         .style("stroke-dasharray", "3,5");
 
     // Guidelines text
-    svg.append('g')
+    let g_text = svg.append('g')
+    g_text
         .selectAll()
         .data(lines)
         .enter()
@@ -416,6 +431,18 @@ function plot_bubble_xp_own_prior() {
         .style("font-size", "x-small")
         .style("fill", "#91d3ff")
         .style("opacity", 0.4);
+
+    g_text.append('text')
+        .attr("x", x(6) + 5)
+        .attr("y", y(-6) - 15)
+        .attr("text-anchor", "left")
+        .attr("alignment-baseline", "middle")
+        .attr("pointer-events", "none")
+        .text("Own%")
+        .style("font-size", "x-small")
+        .style("fill", "#91d3ff")
+        .style("opacity", 0.4);
+
 
     // All players
     svg.append('g')
@@ -470,6 +497,220 @@ function plot_bubble_xp_own_prior() {
 }
 
 function plot_bubble_xp_own_posterior() {
+
+    var margin = { top: 40, right: 30, bottom: 45, left: 45 },
+        width = 500 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+
+    var svg = d3.select("#xp_own_posterior").append("svg")
+        // .attr("width", width + margin.left + margin.right)
+        // .attr("height", height + margin.top + margin.bottom)
+        .attr("viewBox", `0 0  ${(width + margin.left + margin.right)} ${(height + margin.top + margin.bottom)}`)
+        .attr("class", "mx-auto d-block")
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add X axis
+    var x = d3.scaleLinear()
+        .domain([0, 8])
+        .range([0, width]);
+    svg.append("g")
+        // .attr("transform", "translate(0," + height + ")")
+        .attr("opacity", 1)
+        .call(d3.axisBottom(x).ticks(5)
+            .tickSize(height))
+        .call(g => g.selectAll(".tick:not(:first-of-type) line")
+            .attr("stroke-opacity", 0.2)
+            .attr("stroke-dasharray", "3,5"))
+        .call(g => g.selectAll(".tick text").attr("dy", 11));
+
+    // Add X axis label:
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height + 40)
+        .attr("font-size", "smaller")
+        .text("Expected Points");
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([-3, 21])
+        .range([height, 0]);
+    svg.append("g")
+        .attr("opacity", 1)
+        .call(d3.axisRight(y)
+            .ticks(8)
+            .tickSize(width))
+        .call(g => g.selectAll(".tick:not(:first-of-type) line")
+            .attr("stroke-opacity", 0.2)
+            .attr("stroke-dasharray", "3,5"))
+        .call(g => g.selectAll(".tick text").attr("x", -15));
+
+    // Add Y axis label:
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -30)
+        .attr("font-size", "smaller")
+        .text("Realized Points");
+
+    var z = d3.scaleLinear()
+        .domain([0, 80])
+        .range([3, 13]);
+
+    svg.selectAll(".domain").attr("stroke-opacity", 0);
+
+    // Tooltip
+    var tooltip = d3.select("body")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip-p2")
+        .style("position", "absolute")
+        .style("background-color", "#343436")
+        .style("color", "white")
+        .style("border", "solid")
+        .style("border-color", "#ffffff82")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+        .style("font-size", "small");
+
+    // Mouse events
+    var mouseover = function(d) {
+        tooltip.style("opacity", 1)
+        d3.select(this)
+            .style("opacity", 1)
+    }
+    var mousemove = function(d) {
+        name_color = "white";
+        own_color = "white";
+        threat_color = "white";
+        if (d[1].squad == true) {
+            name_color = own_color = "#6fcfd6";
+        } else if (d[1].threat == true) {
+            name_color = threat_color = "#de6363";
+        }
+        tooltip
+            .html(`
+                <div class="mx-auto d-block text-center" style="color: ${name_color}">${d[1].web_name}</div>
+                <table class="table table-striped table-sm table-dark mb-0">
+                    <tr><td class="text-right">xP</td><td>${parseFloat(d[1].points_md).toFixed(2)}</td></tr>
+                    <tr><td class="text-right">rP</td><td>${parseInt(d[1].stats.total_points)}</td></tr>
+                    <tr><td class="text-right">Mins</td><td>${d[1].stats.minutes}</td></tr>
+                    <tr><td class="text-right">Own.</td><td>${d[1].ownership}%</td></tr>
+                    <tr><td class="text-right">Price</td><td>£${d[1].price}M</td></tr>
+                </table>
+            `)
+            .style("left", (d3.event.pageX + 15) + "px")
+            .style("top", (d3.event.pageY + 15) + "px")
+    }
+    var mouseleave = function(d) {
+        tooltip.style("opacity", 0)
+        d3.select(this)
+            .style("opacity", 0.5);
+        tooltip.style("left", "0px")
+            .style("top", "0px");
+    }
+
+    // Guidelines
+    lines = [0.5, 1, 1.5, 2];
+    svg.append('g')
+        .selectAll()
+        .data(lines)
+        .enter()
+        .append('line')
+        .attr("x1", x(0))
+        .attr("y1", y(0))
+        .attr("x2", x(8))
+        .attr("y2", function(d) {
+            return y(8 * d)
+        })
+        .style("stroke", "#91d3ff")
+        .style("stroke-width", 1)
+        .style("opacity", 0.5)
+        .style("stroke-dasharray", "3,5");
+
+    // Guidelines text
+    let g_text = svg.append('g');
+    g_text
+        .selectAll()
+        .data(lines)
+        .enter()
+        .append('text')
+        .attr("x", x(8) + 5)
+        .attr("y", function(d) {
+            return y(8 * d);
+        })
+        .attr("text-anchor", "left")
+        .attr("alignment-baseline", "middle")
+        .attr("pointer-events", "none")
+        .text(function(d) { return d; }) //((d - 1) * 100).toFixed(0) + "%" })
+        .style("font-size", "x-small")
+        .style("fill", "#91d3ff")
+        .style("opacity", 0.4);
+
+    g_text.append('text')
+        .attr("x", x(8) + 5)
+        .attr("y", y(21) - 15)
+        .attr("text-anchor", "left")
+        .attr("alignment-baseline", "middle")
+        .attr("pointer-events", "none")
+        .text("rP/xP")
+        .style("font-size", "x-small")
+        .style("fill", "#91d3ff")
+        .style("opacity", 0.4);
+
+    // All players
+    svg.append('g')
+        .selectAll()
+        .data(app.prior_data.slice(0, -5).filter(i => (i[1].squad == false)))
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return x(d[1].points_md); })
+        .attr("cy", function(d) { return y(parseInt(d[1].stats.total_points)); })
+        .attr("r", function(d) { return z(parseFloat(d[1].ownership)); })
+        .style("fill", "#616362")
+        .style("opacity", "0.5")
+        .attr("stroke", "#9e9e9e")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
+
+    // Dangerous players
+    svg.append('g')
+        .selectAll()
+        .data(app.prior_data.slice(-5))
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return x(d[1].points_md); })
+        .attr("cy", function(d) { return y(parseInt(d[1].stats.total_points)); })
+        .attr("r", function(d) { return z(parseFloat(d[1].ownership)); })
+        .style("fill", "#e22f2f")
+        .style("opacity", "0.5")
+        .attr("stroke", "#fffe53")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
+
+    // Squad
+    svg.append('g')
+        .selectAll()
+        .data(app.prior_data.filter(i => (i[1].squad == true)))
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return x(d[1].points_md); })
+        .attr("cy", function(d) { return y(parseInt(d[1].stats.total_points)); })
+        .attr("r", function(d) { return z(parseFloat(d[1].ownership)); })
+        .style("fill", "#6fcfd6")
+        .style("opacity", "0.5")
+        .attr("stroke", "#ffffff")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
+
+
 
 }
 
