ec821b0db3d11a27ccc43d636f04c3cc62e70804
Sertalp B. Cay <sertalpbilal@gmail.com>
2020-12-29 20:47:21 -0500

    Fix for manual job

--- src/sample.py
+++ src/sample.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from collect import sample_fpl_teams
+import os
 
 if __name__ == "__main__":
-    sample_fpl_teams()
+    gw = os.environ.get('GW', None)
+    sample_fpl_teams(gw)
