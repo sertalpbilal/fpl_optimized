03158fe29cd6040035c7309ba7f8345ac78399f5
Sertalp B. Cay <sertalpbilal@gmail.com>
2021-02-03 15:38:21 -0500

    mend
    Continue even if it fails [skip ci]

--- src/sample.py
+++ src/sample.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
-from collect import sample_fpl_teams
+# from collect import sample_fpl_teams
+from collect import create_folders
 import os
 
+# if __name__ == "__main__":
+#     gw = os.environ.get('GW', None)
+#     sample_fpl_teams(gw)
+
 if __name__ == "__main__":
-    gw = os.environ.get('GW', None)
-    sample_fpl_teams(gw)
+    input_folder, output_folder = create_folders()
+    print(input_folder)
