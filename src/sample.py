c8135ab6392c8ec5d3f6983dbea04af941c91052
Sertalp B. Cay <sertalpbilal@gmail.com>
2021-02-03 15:40:52 -0500

    Reverted changes [skip ci]

--- src/sample.py
+++ src/sample.py
@@ -4,10 +4,6 @@
 from collect import create_folders
 import os
 
-# if __name__ == "__main__":
-#     gw = os.environ.get('GW', None)
-#     sample_fpl_teams(gw)
-
 if __name__ == "__main__":
-    input_folder, output_folder = create_folders()
-    print(input_folder)
+    gw = os.environ.get('GW', None)
+    sample_fpl_teams(gw)
