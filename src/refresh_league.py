6481e22ae7364b578bd32383af237931a1e32e2f
Sertalp B. Cay <sertalpbilal@gmail.com>
2021-05-23 22:20:56 -0400

    Add folder name for season review fix [skip ci]

--- src/refresh_league.py
+++ src/refresh_league.py
@@ -3,5 +3,5 @@
 from collect import create_folders, get_fpl_analytics_league
 
 if __name__ == "__main__":
-    input_folder, output_folder = create_folders()
+    input_folder, output_folder, season_folder = create_folders()
     get_fpl_analytics_league(input_folder)
