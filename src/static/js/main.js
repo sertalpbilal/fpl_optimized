7ac32576de8cf3f209fc0acbc3f8c6fa5d50165d
Sertalp B. Cay <sertalpbilal@gmail.com>
2021-09-19 20:49:17 -0400

    Add download button [skip opt]

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -548,3 +548,33 @@ function millisecondsToStr(milliseconds) {
 
     return "< 1 second";
 }
+
+function exportTableToCSV(selector, filename) {
+    var csv = [];
+    var rows = document.querySelectorAll(selector + " tr");
+    
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        csv.push(row.join(","));        
+    }
+    downloadCSV(csv.join("\n"), filename);
+}
+
+function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    downloadLink = document.createElement("a");
+    downloadLink.download = filename;
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    setTimeout(() => {
+        downloadLink.remove()
+    }, 500)
+}
