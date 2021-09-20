b5d5f8086337a8b8c623a85f93df75fdd3874327
Github Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-09-20 20:30:32 +0000

    Automated build 1255069981

--- static/js/analytics_league.js
+++ static/js/analytics_league.js
@@ -11,7 +11,13 @@ var app = new Vue({
         xp_data: [],
         fixture_data: [],
         autosub_dict: {},
-        rp_by_id: {}
+        rp_by_id: {},
+        //
+        selected_team: undefined,
+        //
+        processed_league_data_cached: [],
+        processed_league_data_cached_last_sort: undefined,
+        highlight_circle: undefined
     },
     computed: {
         this_gw_dynamic() {
@@ -28,9 +34,11 @@ var app = new Vue({
             return 38
         },
         el_data() {
+            if (_.isEmpty(this.static_data)) { return []}
             return this.static_data['elements']
         },
         el_data_by_id() {
+            if (_.isEmpty(this.static_data)) { return []}
             return Object.fromEntries((this.static_data['elements']).map(i => [i.id, i]))
         },
         xp_by_id() {
@@ -38,7 +46,7 @@ var app = new Vue({
             return Object.fromEntries(xp_data)
         },
         processed_league_data() {
-            if (_.isEmpty(this.league_data) || _.isEmpty(this.xp_data) || _.isEmpty(this.rp_data) || _.isEmpty(this.rp_by_id)) { return []}
+            if (_.isEmpty(this.league_data) || _.isEmpty(this.el_data_by_id) || _.isEmpty(this.xp_data) || _.isEmpty(this.rp_data) || _.isEmpty(this.rp_by_id)) { return []}
 
             let t = this.league_data
             t.forEach((t) => {
@@ -72,19 +80,43 @@ var app = new Vue({
                 last_pts = rounded(t.info.pre_xp,3)
             })
 
+            this.processed_league_data_cached = t
+
             return t
+        },
+        team_list() {
+            if (_.isEmpty(this.processed_league_data)) { return []}
+            let teams = this.processed_league_data.map(i => { return {'entry': i[0].entry, 'entry_name': i[0].entry_name, 'player_name': i[0].player_name}})
+            return teams
+        },
+        team_list_sorted_entry_name() {
+            return _.orderBy(this.team_list, ['entry_name'], 'asc')
+        },
+        team_list_sorted_player_name() {
+            return _.orderBy(this.team_list, ['player_name'], 'asc')
+        },
+        selected_entry_ref() {
+            if (_.isEmpty(this.processed_league_data_cached)) { return {}}
+            if (!this.selected_team) {return {}}
+            return this.processed_league_data_cached.find(i => i[0].entry == this.selected_team)
         }
     },
     methods: {
         active_gw_update(val) {
             
-            $("#league_table").DataTable().destroy()
+            // $("#league_table").DataTable().destroy()
             this.league_data = []
+            jQuery("#xp_vs_rp").empty()
 
             this.active_gw = this.active_gw + parseInt(val)
 
             let cgw = this.active_gw
 
+            this.static_data = undefined
+            this.league_data = []
+            this.fixture_data = []
+            this.rp_data = []
+            this.xp_data =[]
 
             Promise.all([
                 get_fpl_main_data().then((data) => {
@@ -107,6 +139,7 @@ var app = new Vue({
                 app.autosub_dict = generate_autosub_dict(app.el_data, app.rp_by_id)
         
                 app.$nextTick(() => {
+                    draw_xp_vs_rp()
                     app.refresh_table()
                 })
             })
@@ -114,30 +147,350 @@ var app = new Vue({
         },
         refresh_table() {
 
-            $("#league_table").DataTable().destroy();
-            this.$nextTick(() => {
-                $("#league_table").DataTable({
-                    "order": [
-                        [0, 'asc']
-                    ],
-                    // "lengthChange": true,
-                    "lengthMenu": [5, 10, 25, 50, 100],
-                    "pageLength": 10,
-                    "searching": true,
-                    "info": false,
-                    "paging": true,
-                    // paging: false,
-                    // scrollY: '50vh',
-                    "columnDefs": [
-                        { targets: [1], orderable: false },
-                    ],
-                    // responsive: true,
-                    "scrollX": true
-                });
-            })
+        },
+        sortData(e) {
+            let tag = e.currentTarget.dataset.tag
+            let source = jQuery(e.currentTarget).closest('table').data('source')
+            let last_sort = this[source + "_last_sort"]
+            let new_order
+            let new_value
+            if (tag == last_sort){
+                new_order = _.orderBy(this[source], tag, 'desc')
+                new_value = undefined
+            }
+            else {
+                new_order = _.orderBy(this[source], tag, 'asc')
+                new_value = tag
+            }
+            this[source] = new_order
+            this[source + "_last_sort"]  = new_value 
+        },
+        selectTeam(e) {
+            debugger
         }
     }
 });
+
+
+function draw_xp_vs_rp() {
+
+    return new Promise((resolve, reject) => {
+
+        if (_.isEmpty(app.processed_league_data)) { resolve("Not ready"); }
+
+        var margin = { top: 20, bottom: 20, left: 20, right: 10 },
+            width = 400 - margin.left - margin.right,
+            height = 240 - margin.top - margin.bottom
+
+        jQuery("#xp_vs_rp").empty()
+
+        let cnv = d3.select("#xp_vs_rp")
+            .append("svg")
+            .attr("id", "xp_vs_rp_svg")
+            .attr("viewBox", `0 0  ${(width + margin.left + margin.right)} ${(height + margin.top + margin.bottom)}`)
+            .style('display', 'block')
+            .style('min-width', '300px')
+
+        cnv.append("defs").append("SVG:clipPath")
+            .attr("id", "clip1")
+            .append("SVG:rect")
+            .attr("width", width )
+            .attr("height", height )
+            .attr("x", 0)
+            .attr("y", 0);
+
+        let svg = cnv.append('g').attr('class', 'svg-actual').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        let content = svg.append('g').attr('clip-path', 'url(#clip1)')
+        let grayrect = content.append('g').attr('class', 'brush');
+        grayrect.append('rect').attr('fill', '#5a5d5c').attr('width', width).attr('height', height).attr("stroke", "white").attr("stroke-width", "0.5");
+
+        let data = app.processed_league_data
+
+        
+        // Min max values
+        let x_high = Math.max(...data.map(i => i.info.post_xp)) * 1.1
+        let x_low = 0.5
+
+        // Axis-x
+        var x = d3.scaleLinear()
+            .domain([x_low, x_high])
+            .range([0, width]);
+        let xAxis = svg.append("g")
+            .attr("opacity", 1)
+            .call(d3.axisBottom(x)
+                .tickSize(height)
+            )
+            .call(g => g.selectAll(".tick line")
+                .attr("stroke-opacity", 0.1)
+                .attr("stroke-width", 0.5)
+                .attr("stroke-dasharray", "3,1")
+                .style('pointer-events', 'none')
+                )
+            // .call(g => g.selectAll(".tick text").attr("dy", 11)
+            // );
+
+        // Add X axis label:
+        svg.append("text")
+            .attr("text-anchor", "middle")
+            .attr("x", width / 2)
+            .attr("y", height + 15)
+            .attr("font-size", "4pt")
+            .attr("fill", "white")
+            .text("Expected/Projected Points");
+
+        // Axis-y
+        let y_high = Math.max(...data.map(i => i.info.rp)) * 1.1
+        let y_low = 0
+        var y = d3.scaleLinear().domain([y_low, y_high]).range([height, 0]);
+        let yAxis = svg.append('g')
+            .attr("transform", "translate(" + width + ",0)")
+            .call(d3.axisLeft(y).tickSize(width))
+            .call(g => g.selectAll(".tick text"))
+            .call(g => g.selectAll(".tick:first-of-type line").style("display", "none"));
+
+        svg.call(g => g.selectAll(".tick text")
+                .attr("fill", "white"))
+            .call(g => g.selectAll(".tick line")
+                .attr("stroke-dasharray", "3,1")
+                .attr("stroke-width", 0.5)
+                .attr("stroke-opacity", 0.1)
+                .style('pointer-events', 'none'))
+            .call(g => g.selectAll(".domain")
+                .attr("opacity", 0))
+            
+        // Add y axis label:
+        svg.append("text")
+            .attr("text-anchor", "left")
+            .attr("x", -margin.left)
+            .attr("y", -5)
+            .attr("font-size", "4pt")
+            .attr("fill", "white")
+            .text("Realized Points");
+
+        // Title
+        // svg.append("text")
+        //     .attr("text-anchor", "middle")
+        //     .attr("alignment-baseline", "center")
+        //     .attr("dominant-baseline", "center")
+        //     .attr("x", width / 2)
+        //     .attr("y", -15)
+        //     .attr("font-size", "4.5pt")
+        //     .attr("fill", "white")
+        //     .text("Expected Points versus Effective Ownership (Log)");
+
+        let if_mobile = window.screen.width < 700
+
+        let dblclick_ctr = null
+        let delete_ctr = () => { dblclick_ctr = null }
+
+        // ANIMATION ON SELECT
+        let updateChart = (e, d) => {
+
+            debugger
+
+            let extent = e.selection
+
+            if (!e.sourceEvent) { return }
+
+            grayrect.call(brush.move, null)
+
+            let scale = 1
+
+            if (!extent) {
+                if (!dblclick_ctr) { return dblclick_ctr = setTimeout(delete_ctr, 350) }
+                x.domain([x_low, x_high])
+                y.domain([y_low, y_high])
+            }
+            else {
+                x.domain([x.invert(extent[0][0]), x.invert(extent[1][0])])
+                y.domain([y.invert(extent[1][1]), y.invert(extent[0][1])])
+                scale = Math.min(
+                    (width) / (extent[1][0] - extent[0][0]),
+                    (height) / (extent[1][1] - extent[0][1]),
+                    )
+            }
+
+
+            xAxis.transition().duration(1000)
+                .call(d3.axisBottom(x)
+                    .tickSize(height)
+                )
+                .call(g => g.selectAll(".tick line")
+                    .attr("stroke-opacity", 0.1)
+                    .attr("stroke-width", 0.5)
+                    .attr("stroke-dasharray", "3,1")
+                    .style('pointer-events', 'none')
+                )
+                
+            yAxis.transition().duration(1000)
+                .call(d3.axisLeft(y).tickSize(width))
+                .call(g => g.selectAll(".tick text"))
+                .call(g => g.selectAll(".tick:first-of-type line").style("display", "none"))
+
+            svg.call(g => g.selectAll(".tick text")
+                .attr("fill", "white"))
+                .call(g => g.selectAll(".tick line")
+                    .attr("stroke-dasharray", "3,1")
+                    .attr("stroke-width", 0.5)
+                    .attr("stroke-opacity", 0.1)
+                    .style('pointer-events', 'none'))
+                .call(g => g.selectAll(".domain")
+                    .attr("opacity", 0))
+                .call(g => g.selectAll(".tick").attr("font-size", "4pt"));
+
+            managers.selectAll("circle")
+                .transition().duration(1000)
+                .attr("cx", (d) => x(d.info.post_xp))
+                .attr("cy", (d) => y(d.info.rp))
+                // .attr("r", (d) => scale * (if_mobile ? 3.5 : 2))
+
+            svg.selectAll(".manager_text").transition().duration(1000)
+                .attr("x", function() {
+                    debugger
+                    let a = d3.select(this)
+                    let mid = a.node().dataset['mid']
+                    return x(app.processed_league_data.find(i => i[0].entry == mid).info.post_xp)
+                })
+                .attr("y", function() {
+                    debugger
+                    let a = d3.select(this)
+                    let mid = a.node().dataset['mid']
+                    return y(app.processed_league_data.find(i => i[0].entry == mid).info.rp) - 5
+                })
+                
+
+        }
+
+        let brush = d3.brush()
+            .extent([[0,0], [width,height]])
+            // .on("brush", brushed)
+            .on("end", updateChart)
+
+        grayrect.call(brush)
+
+
+
+        
+
+        svg.call(s => s.selectAll(".tick").attr("font-size", "4pt"));
+
+        let managers = content.selectAll()
+            .data(data)
+            .enter()
+
+        let mouseover = (e) => {
+            e.stopPropagation()
+            let pid = e.currentTarget.dataset['id']
+            let entry = data.find(i => i[0].entry == pid)
+            $(".hovertext").remove()
+            let t = d3.select(e.target)
+            svg.append("text")
+                .attr("class", "hovertext")
+                .attr("text-anchor", "middle")
+                .attr("alignment-baseline", "center")
+                .attr("dominant-baseline", "center")
+                .attr("x", t.attr("cx"))
+                .attr("y", parseFloat(t.attr("cy")) - 5)
+                .attr("font-size", "3pt")
+                .style("pointer-events", "none")
+                .attr("fill", "white")
+                .text(entry[0].entry_name);
+        }
+
+        // let toggleClick = (e) => {
+        //     e.stopPropagation()
+        //     let pid = e.currentTarget.dataset['id']
+        //     let entry = app.element_data_combined[pid]
+        //     let t = d3.select(e.target)
+        //     if ($("#p-" + entry.id).length == 0) {
+        //         svg.append("text")
+        //             .attr("id", "p-" + entry.id )
+        //             .attr("text-anchor", "middle")
+        //             .attr("alignment-baseline", "center")
+        //             .attr("dominant-baseline", "center")
+        //             .attr("x", t.attr("cx"))
+        //             .attr("y", parseFloat(t.attr("cy")) - 5)
+        //             .attr("font-size", "3pt")
+        //             .style("pointer-events", "none")
+        //             .attr("fill", "white")
+        //             .text(entry.name);
+        //     }
+        //     else {
+        //         $("#p-" + entry.id).remove()
+        //     }
+        // }
+
+        let color_it = (mid, toggle=false) => {
+            let existing = d3.select(".selected-circle").node()
+            if (existing){ existing.classList.remove("selected-circle") }
+            let selected = d3.select(`*[data-id="${mid}"]`)
+            selected.node().classList.add("selected-circle")
+            selected.raise()
+
+            if (!toggle) {
+                jQuery(".manager_text").remove()
+            }
+
+            if ($("#p-" + mid).length == 0) {
+                svg.append("text")
+                    .attr("id", "p-" + mid )
+                    .attr("class", "manager_text")
+                    .attr("text-anchor", "middle")
+                    .attr("alignment-baseline", "center")
+                    .attr("dominant-baseline", "center")
+                    .attr("data-mid", mid)
+                    .attr("x", selected.attr("cx"))
+                    .attr("y", parseFloat(selected.attr("cy")) - 5)
+                    .attr("font-size", "3pt")
+                    .style("pointer-events", "none")
+                    .attr("fill", "white")
+                    .text(data.find(i => i[0].entry == mid)[0].entry_name);
+            }
+            else if (toggle) {
+                $("#p-" + mid).remove()
+            }
+        } 
+
+        let mouseleave = (e) => {
+            e.stopPropagation()
+            $(".hovertext").remove()
+        }
+
+        let mouseclick = (e) => {
+            e.stopPropagation()
+            let mid = e.currentTarget.dataset['id']
+            app.selected_team = mid
+
+            // highlight
+            color_it(mid=mid, toggle=true)
+        }
+
+        managers
+            .append("circle")
+            .attr("class", "analytics_circles")
+            .attr("cx", (d) => x(d.info.post_xp))
+            .attr("cy", (d) => y(d.info.rp))
+            .attr("r", if_mobile ? 3.5 : 2)
+            .attr("data-id", (d) => d[0].entry)
+            .on("mouseover", mouseover)
+            .on("click", mouseclick)
+            .on("mouseleave", mouseleave)
+            ;
+
+
+        app.highlight_circle = (e) => {
+            let team_id = e.target.value
+            color_it(team_id, toggle=false)
+        }
+
+        if (app.selected_team) {
+            app.highlight_circle({'target': {'value': app.selected_team}})
+        }
+
+        resolve()
+
+    });
+}
 
 $(document).ready(() => {
     let cgw = parseInt(gw.slice(2))
@@ -162,6 +515,7 @@ $(document).ready(() => {
         app.autosub_dict = generate_autosub_dict(app.el_data, app.rp_by_id)
 
         app.$nextTick(() => {
+            draw_xp_vs_rp()
             app.refresh_table()
         })
     })
