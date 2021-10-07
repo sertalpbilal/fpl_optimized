491b8d5f11772dd1649df2e799833bea9a40152e
Github Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-10-07 15:31:44 +0000

    Automated build 1316837049

--- static/js/main.js
+++ static/js/main.js
@@ -606,3 +606,20 @@ function downloadCSV(csv, filename) {
         downloadLink.remove()
     }, 500)
 }
+
+function saveAs(uri, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download === 'string') {
+        link.href = uri;
+        link.download = filename;
+        //Firefox requires the link to be in the body
+        document.body.appendChild(link);
+        //simulate click
+        link.click();
+        //remove the link when done
+        document.body.removeChild(link);
+    } else {
+        window.open(uri);
+    }
+}
+
