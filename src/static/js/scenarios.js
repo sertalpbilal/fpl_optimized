a2acc3006597a5f03b1d5397b9635288048c6c4b
Sertalp B. Cay <sertalpbilal@gmail.com>
2022-03-03 10:24:58 -0500

    Move the team create functionality to main, fix gw switch bug [skip opt]

--- src/static/js/scenarios.js
+++ src/static/js/scenarios.js
@@ -347,75 +347,11 @@ var app = new Vue({
     },
     methods: {
         set_team_with_url(sorted, picks, cap, vice_cap) {
-            if (sorted) {
-                let team = {}
-                team['picks'] = []
-                picks.forEach((v,i) => {
-                    team.picks.push({
-                        'element': v,
-                        'multiplier': v == cap ? 2 : i <= 10 ? 1 : 0,
-                        'is_captain': v == cap,
-                        'is_vice_captain': v == vice_cap,
-                        'position': i+1
-                    })
-                })
-
-                this.team_data = _.cloneDeep(team)
-                this.loading = false
-                draw_histogram()
-            }
-            else {
-                let team = {}
-                team['picks'] = []
-                let picks_with_xp = picks.map(i => [i, this.elements_dict[i].element_type, this.sc_player_averages.avg[i] || 0, this.elements_dict[i].web_name, (this.sc_player_averages.app[i] || 0) > 70 ? 1 : 0, this.sc_player_averages.pps[i] || 0])
-                let ordered_picks = _.orderBy(picks_with_xp, ['4', '5'], ['desc', 'desc'])
-                // 1) sort by if played over 60 + points per scenario
-                let position_bounds = {
-                    // 1: {'min': 1, 'max': 1},
-                    2: {'min': 3, 'max': 5},
-                    3: {'min': 2, 'max': 5},
-                    4: {'min': 1, 'max': 3}
-                }
-
-                let main_gk;
-                let bench_gk;
-                let lineup = [];
-                let bench = [];
-
-                main_gk = ordered_picks.filter(i => i[1] == 1)[0][0]
-                bench_gk = ordered_picks.filter(i => i[1] == 1)[1][0]
-
-                for (let pos in position_bounds) {
-                    if (pos == 1) { continue }
-                    let picked = ordered_picks.filter(i => i[1] == pos).slice(0, position_bounds[pos].min)
-                    picked.forEach((i) => lineup.push(i[0]))
-                }
-                // 2) then use averages for remaining players
-                let remaining = _.orderBy(ordered_picks.filter(i => !lineup.includes(i[0]) && i[1] != 1), '2', 'desc')
-                for (let i=lineup.length; i<10; i++) {
-                    lineup.push(remaining.shift()[0])
-                }
-                bench = remaining.map(i => i[0])
-
-                cap = ordered_picks[0][0]
-                vice_cap = ordered_picks[0][1] == 1 && ordered_picks[1][1] == 1 ? ordered_picks[2][0] : ordered_picks[1][0]
-                let squad = [main_gk].concat(lineup).concat([bench_gk]).concat(bench)
-
-                squad.forEach((v,i) => {
-                    team.picks.push({
-                        'element': v,
-                        'multiplier': v == cap ? 2 : i <= 10 ? 1 : 0,
-                        'is_captain': v == cap,
-                        'is_vice_captain': v == vice_cap,
-                        'position': i+1
-                    })
-                })
-
-                this.team_data = _.cloneDeep(team)
-                this.loading = false
-                draw_histogram()
-                
-            }
+            let pids = this.elements.map(i => i.id)
+            let xp_data = Object.fromEntries(pids.map(i => [i, [this.sc_player_averages.avg[i] || 0, (this.sc_player_averages.app[i] > 70 || 0) ? 1 : 0, this.sc_player_averages.pps[i] || 0]]))
+            this.team_data = createTeamFromList(sorted, picks, cap, vice_cap, this.elements_dict, xp_data)
+            this.loading = false
+            draw_histogram()
         },
         fetch_team_picks() {
             this.team_data = undefined
@@ -1045,7 +981,7 @@ function draw_histogram() {
     })
 
     let mpts = 0
-    if (!app.is_next_gw) {
+    if (!app.is_next_gw && app.team_id != '') {
         mpts = app.team_data.entry_history.points
     }
 
@@ -1195,7 +1131,7 @@ function draw_histogram() {
 
 
     // When plotting old GW: draw a bar!
-    if (!app.is_next_gw) {
+    if (!app.is_next_gw && app.team_id != '') {
         let pts = app.team_data.entry_history.points
 
         // holder.append("rect")
@@ -1262,7 +1198,7 @@ $(document).ready(() => {
             let captain = params.get('cap')
             let vice_cap = params.get('vc')
             app.set_team_with_url(sorted, picks, captain, vice_cap)
-            app.team_id = 1
+            // app.team_id = 1
         }
         app.ready = true
     })
